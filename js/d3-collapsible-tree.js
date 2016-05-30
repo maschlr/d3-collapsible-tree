920a228616d0f25ebdcea9d77bb37f2a32034d8f
Marten Schlueter <marten.schlueter@gmail.com>
2016-05-30 17:49:43 +0200

    Typos, bugs; seems to work now

--- js/d3-collapsible-tree.js
+++ js/d3-collapsible-tree.js
@@ -8,50 +8,73 @@
                 right: 120,
                 bottom: 20,
                 left: 120
-            }
+            };
         } else {
             this.margin = opts.margin;
         }
-        this.height = opts.height || 800 - margin.top - margin.bottom;
-        this.width = opts.width || 960 - margin.right - margin.left;
+        this.height = opts.height || 800 - this.margin.top - this.margin.bottom;
+        this.width = opts.width || 960 - this.margin.right - this.margin.left;
         this.i = 0;
         this.duration = 750;
 
         this.tree = d3.layout.tree()
             .size([this.height, this.width]);
 
-        this.diagonal = d3.svg.diagonal()
-            .projection(d => [d.y, d.x]);
-
         this.svg = d3.select(this.element).append('svg')
             .attr('width', this.width + this.margin.left + this.margin.right)
             .attr('height', this.height + this.margin.top + this.margin.bottom)
             .append('g')
-            .attr('transform', 'translate(' + magin.left + ',' + margin.top + ')');
+            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
     };
 
-    Tree.prototype.getData = function(url) {
-        d3.json(url, function(error, data) {
-            if (error) throw error;
-
-            this.setData(data);
-        });
-    }
+    Tree.prototype.diagonal = d3.svg.diagonal()
+        .projection(function(d) {return [d.y, d.x];});
 
     Tree.prototype.setData = function(data) {
         this.root = data;
         this.root.x0 = this.height / 2;
-        this.y0 = 0;
+        this.root.y0 = 0;
 
-        this.root.children.forEach(this.collapse);
-        this.update(this.root)
-    }
+        // Recursively collapse the tree
+        function collapse(node) {
+            if (node.children) {
+                node._children = node.children;
+                node._children.forEach(collapse);
+                node.children = null;
+            }
+        }
+
+        this.root.children.forEach(collapse);
+        this.update(this.root);
+    };
+
+    Tree.prototype.getData = function(url) {
+        var _this = this;
+        d3.json(url, function(error, data) {
+            if (error) throw error;
+
+            _this.setData(data);
+        });
+    };
 
     Tree.prototype.update = function(source) {
+        var _this = this;
+
+        // Toggle children on click
+        function click(d) {
+            if (d.children) {
+                d._children = d.children;
+                d.children = null;
+            } else {
+                d.children = d._children;
+                d._children = null;
+            }
+            _this.update(d);
+        }
 
         // Compute new tree layout
         var nodes = this.tree.nodes(this.root).reverse(),
-            links = this.tree.links(this.nodes);
+            links = this.tree.links(nodes);
 
         // Normalize for fixed-depth
         nodes.forEach(function(d) {
@@ -60,39 +83,33 @@
 
         // Update the nodes
         var node = this.svg.selectAll("g.node")
-            .data(nodes, d => d.id || (d.id = ++i));
+            .data(nodes, function(d) {return d.id || (d.id = ++_this.i);});
 
         // Enter any new nodes at the parent's previous position
         var nodeEnter = node.enter().append('g')
             .attr('class', 'node')
-            .attr('transform', d => 'translate(' + source.y0 + ',' + source.x0 ')')
-            .on('click', this.click);
+            .attr('transform', function(d) { return 'translate(' + source.y0 + ',' + source.x0 + ')';})
+            .on('click', click);
 
         nodeEnter.append('circle')
             .attr('r', 1e-6)
-            .style('fill', d => d_children ? 'lightstellblue' : '#fff');
+            .style('fill', function(d) {return d._children ? 'lightstellblue' : '#fff';});
 
         nodeEnter.append('text')
-            .attr('x'
-                d => d.children || d._children ? -10 : 10;)
-            .attr('dy'
-                '.35em')
-            .attr('text-anchor'
-                d => d.children || d._children ? "end" : "start")
-            .text(d => d.name)
+            .attr('x', function(d) {return d.children || d._children ? -10 : 10;})
+            .attr('dy', '.35em')
+            .attr('text-anchor', function(d) {return d.children || d._children ? "end" : "start";})
+            .text(function(d) {return d.name;})
             .style('fill-opacity', 1e-6);
 
         // Transition nodes to their new postition
         var nodeUpdate = node.transition()
             .duration(this.duration)
-            .attr('transform'
-                d => 'translate(' + d.y + ',' + d.x + ')');
+            .attr('transform', function(d) {return 'translate(' + d.y + ',' + d.x + ')';});
 
         nodeUpdate.select('circle')
-            .attr('r'
-                4.5)
-            .style('fill'
-                d => d._children ? 'lightsteelblue' : '#fff');
+            .attr('r', 4.5)
+            .style('fill', function(d) {return d._children ? 'lightsteelblue' : '#fff';});
 
         nodeUpdate.select('text')
             .style('fill-opacity', 1);
@@ -125,7 +142,7 @@
                     x: source.x0,
                     y: source.y0
                 };
-                return diagonal({
+                return _this.diagonal({
                     source: o,
                     target: o
                 });
@@ -144,7 +161,7 @@
                     x: source.x,
                     y: source.y
                 };
-                return this.diagonal({
+                return _this.diagonal({
                     source: o,
                     target: o
                 });
@@ -156,32 +173,16 @@
             d.x0 = d.x;
             d.y0 = d.y;
         });
+    };
+
+
+
+    if (typeof define === "function" && define.amd) {
+        this.Tree = Tree;
+        define(Tree);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = Tree;
+    } else {
+        this.Tree = Tree;
     }
-
-    // Recursively collapse the tree
-    Tree.prototype.collapse = function(node) {
-        if (node.children) {
-            node._children = node.children;
-            node._children.forEach(this.collapse);
-            node.children = null;
-        }
-    }
-
-    // Toggle children on click
-    Tree.prototype.click = function(node) {
-        if (node.children) {
-            node._children = node.children;
-            node.children = null;
-        } else {
-            node.children = node._children;
-            node._children = null;
-        }
-        this.update(node);
-    }
-
-
-
-    if (typeof define === "function" && define.amd) this.Tree = Tree, define(Tree);
-    else if (typeof module === "object" && module.exports) module.exports = Tree;
-    else this.Tree = Tree;
 }();
